3a5b5eaa1ef2eeb22c7763557f611bb10647a59d
nyomathys <nyo.mathys@student.ehb.be>
2024-03-20 22:23:17 +0100

    enkele arrow fuctions toegevoegd aan de page

---
+++ JavaScripts/ArrowFunction.js
@@ -0,0 +1,18 @@
+let clickedButton1 = false;
+
+document.getElementById("button1").addEventListener("click", () => {
+  clickedButton1 = true;
+});
+
+const greetUser = (name) => {
+  if (clickedButton1) {
+    alert(`Hallo, ${name}! Waarom heb je op die ene knop geklikt?! Ik had je gevraagd om dit niet te doen!`);
+  } else {
+    alert(`Hallo, ${name}! Bedankt om niet op de kleine knop te klikken!`);
+  }
+};
+
+document.getElementById("button2").addEventListener("click", () => {
+  const name = document.getElementById("naam").value;
+  greetUser(name);
+});
