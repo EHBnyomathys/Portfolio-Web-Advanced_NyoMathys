874eaf6be4ca146e1bf4fabd3656093124ab381a
nyomathys <nyo.mathys@student.ehb.be>
2024-04-17 22:17:23 +0200

    afgemaakt

---
+++ JavaScripts/TemplateLiterals&Destructing.js
@@ -0,0 +1,8 @@
+const user = {
+  name: 'Nyo',
+  age: 19,
+  city: 'Brussel'
+};
+const { name, age, city } = user;
+const message = `Hallo, mijn naam is ${name}. Ik ben ${age} jaar oud en ik woon in ${city}.`;
+console.log(message);
