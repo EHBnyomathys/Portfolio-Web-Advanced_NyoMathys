722fc54f0ce874035aa9477a05e417e207f79fce
nyomathys <nyo.mathys@student.ehb.be>
2024-04-22 19:13:06 +0200

    kleine functie toegevoegd aan de 'flexbox css grid' knop

---
+++ JavaScripts/Main.js
@@ -0,0 +1,3 @@
+document.getElementById('flexboxGridButton').addEventListener('click', function() {
+  alert('Maak eens je venster grooter en kleiner, je zal dan het effect van de flexbox ervaren!');
+});
