f09954fd34d7cf874560e5ede268ac9650f94dc2
nyomathys <nyo.mathys@student.ehb.be>
2024-05-07 12:03:24 +0200

    extra informatie gezet in de console bij destructing&templateLiterals

--- JavaScripts/TemplateLiterals&Destructing.js
+++ JavaScripts/TemplateLiterals&Destructing.js
@@ -4,5 +4,5 @@ const user = {
   city: 'Brussel'
 };
 const { name, age, city } = user;
-const message = `Hallo, mijn naam is ${name}. Ik ben ${age} jaar oud en ik woon in ${city}.`;
+const message = `Hallo, mijn naam is ${name}. Ik ben ${age} jaar oud en ik woon in ${city}.\nIn de code word 'name', 'age' en 'city' opgehaald van het object met destructing. Daarna worden deze gebruikt in een template literal om het begroetingsbericht te maken.`;
 console.log(message);
