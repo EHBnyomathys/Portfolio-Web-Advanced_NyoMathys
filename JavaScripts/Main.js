de65a9faeb82b701a19877f142689b89a5407233
nyomathys <nyo.mathys@student.ehb.be>
2024-05-07 15:14:20 +0200

    kleine self executing function toegevoegd die de grbruiker verwelkomt bij het refreshen van de site

--- JavaScripts/Main.js
+++ JavaScripts/Main.js
@@ -1,3 +1,11 @@
 document.getElementById('flexboxGridButton').addEventListener('click', function() {
   alert('Maak eens je venster grooter en kleiner, je zal dan het effect van de flexbox ervaren!');
 });
+document.getElementById('SelfExecButton').addEventListener('click', function() {
+  alert('Refresh de site een keer, de self executing function is de functie die de gebruiker verwelkomt!');
+});
+(function() {
+  window.onload = function() {
+    alert('Welkom op de website van Nyo Mathys!');
+  };
+})();
